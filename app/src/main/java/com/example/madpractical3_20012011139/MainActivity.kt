ce7620462a88a3dd665e7b978e0e0440ce7f30f5
AnilPrajapati2001 <anilprajapati1056@gmail.com>
2022-09-16 00:07:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical3_20012011139/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.madpractical3_20012011139
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madpractical3_20012011139.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        Intent(this, LoginActivity::class.java).also {startActivity(it)}
+    }
+}
